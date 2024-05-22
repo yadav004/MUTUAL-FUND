edb951addf73b902afcc440f957a3d52e7e62a6c
Rahul <121729612+yadav004@users.noreply.github.com>
2024-05-22 13:56:23 +0530

    Add files via upload

---
+++ project/script.js
@@ -0,0 +1,9 @@
+function showBenefits() {
+    const benefits = document.getElementById("benefits");
+    if (benefits.style.display === "none") {
+      benefits.style.display = "block";
+    } else {
+      benefits.style.display = "none";
+    }
+  }
+  
